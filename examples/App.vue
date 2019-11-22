dc20fce72414451ebf5dc5ec39c525e963358b50
chen129 <1378116863@qq.com>
2019-11-22 17:42:11 +0800

    :package: docs更新

--- examples/App.vue
+++ examples/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="title">{{ title }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      title: 'CL-UI'
+    }
   }
 }
 </script>
 
 <style lang="less">
+.title {
+  font-size: 64px;
+  font-weight: bold;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
