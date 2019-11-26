eaad772b4cf5d396524bb40de316986ae09cd2cf
chen129 <1378116863@qq.com>
2019-11-26 16:31:05 +0800

    :rocket: update travis

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-git clone https://${GH_REF} .temp
-cd .temp
-git checkout gh-pages
-cd ../
-mv .temp/.git dist
 cd dist
-git config user.name "${U_NAME}"
-git config user.email "${U_EMAIL}"
+git config user.name "${USER_NAME}"
+git config user.email "${USER_EMAIL}"
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:chen129/cl-ui.git master:gh-pages
+git push -f git@github.com:chen129/cl-ui.git master:${P_BRANCH}
 
 cd -
