53a07adfd5d3504df14c9810d88514dda2b54193
chen129 <1378116863@qq.com>
2019-11-26 15:56:30 +0800

    :rocket: update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 set -e
 
+git checkout gh-pages
 cd dist
-
+git config user.name "${U_NAME}"
+git config user.email "${U_EMAIL}"
 git init
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:chen129/cl-ui.git master:gh-pages
 
 cd -
